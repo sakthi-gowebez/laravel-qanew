cc0cd8e9d6909a90710491edfb20a4aa0f9389c5
webapiguru <sakthi@webapiguru.com>
2019-10-20 10:09:03 -0400

    Generate Question model via artisan model generator

---
+++ app/Question.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
+
+    protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(Users::class);
+
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+
+    }
+}
